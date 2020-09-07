a446b85f30db201a92078a9468f465b9443bdbb8
Daniel Winkler <danielw2904@disroot.org>
2020-09-07 18:06:55 +0200

    added function with docs

--- src/CoolPkg.jl
+++ src/CoolPkg.jl
@@ -1,5 +1,15 @@
 module CoolPkg
 
-# Write your package code here.
+export add2
+
+"""
+    add2(a, b)    
+
+Adds two numbers together...
+
+* `a`: A number
+* `b`: Another number
+"""
+add2(a, b) = a + b
 
 end
