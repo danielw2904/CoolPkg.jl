d68004830a60648437de354bd638475d0c3bd361
Documenter.jl <documenter@juliadocs.github.io>
2020-09-07 16:14:07 +0000

    build based on 7555902

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CoolPkg","category":"page"},{"location":"#CoolPkg","page":"Home","title":"CoolPkg","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CoolPkg]","category":"page"},{"location":"#CoolPkg.add2-Tuple{Any,Any}","page":"Home","title":"CoolPkg.add2","text":"add2(a, b)\n\nAdds two numbers together...\n\na: A number\nb: Another number\n\n\n\n\n\n","category":"method"}]
+}
